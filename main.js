2b17d2b5c1bf56d2e8ffa4786dda36498accfe22
nkushi <nkushi94@hotmail.com>
2024-03-26 16:46:06 +0000

    Added Proof-of-Work

--- main.js
+++ main.js
@@ -7,16 +7,26 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = '';
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
     }
 
     createGenesisBlock(){
@@ -29,7 +39,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -51,13 +61,9 @@ class Blockchain{
 }
 
 let nabeelCoin = new Blockchain();
-nabeelCoin.addBlock(new Block(1, "25/03/2024", {amount: 4}));
-nabeelCoin.addBlock(new Block(2, "26/03/2024", {amount: 10}));
 
-console.log('Is Blockchain valid? ' + nabeelCoin.isChainValid());
+console.log('Mining block 1... ');
+nabeelCoin.addBlock(new Block(1, "25/03/2024", { amount: 4}));
 
-nabeelCoin.chain[1].data = {amount: 1000};
-nabeelCoin.chain[1].hash = nabeelCoin.chain[1].calculateHash();
-console.log('Is Blockchain valid? ' + nabeelCoin.isChainValid());
-
-//console.log(JSON.stringify(nabeelCoin, null, 4))
+console.log('Mining block 2... ');
+nabeelCoin.addBlock(new Block(2, "26/03/2024", { amount: 8}));
